3f200071f485ef7809bf8c014203d64981efb48c
mislost <chenlong0554@qq.com>
2015-01-21 16:43:45 +0800

    add ip html

---
+++ login.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+import sys
+import web
+
+sys.path.append('.')
+import apachexml
+passwd_dict = {"mislost":"admin1234","falls":"123456"}
+render = web.template.render('templates/')
+urls = (
+	'/login', 'index',
+	'/addip', 'addip'
+)
+
+class index:
+	def GET(self):	
+		return render.login()
+
+	def POST(self):
+		apachexml.create_html()
+		i = web.input()
+		username = str(i.username)
+		password = str(i.password)
+		if username in passwd_dict.keys():
+			if password == passwd_dict[username]:
+				return render.show_ip()
+			else:
+		 		return render.login_failed()
+		else:
+			return render.username_errors()
+class addip:
+	def GET(self):
+		return render.addip()
+
+	def POST(self):
+		i = web.input()
+		name = i.name
+		ip = i.ip
+		apachexml.add_ip(ip, name)
+		apachexml.create_html()
+		return render.show_ip()
+
+
+			
+
+
+if __name__ == '__main__':
+	app = web.application(urls, globals())
+	app.run()
+
+	
